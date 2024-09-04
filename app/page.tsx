0ce7f4b606ef06b2866f70bf023bfd871384d05a
Davod <davodnikoyii@gmail.com>
2024-09-04 09:45:04 +0200

    Added register form in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,24 @@
-import { Button } from "@mui/material";
+"use client"
+
+import { Button, Stack, TextField, Typography } from "@mui/material";
 
 export default function Home() {
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    console.log("SUBMITTED");
+  }
+
   return (
     <main >
-     <Button>Hello world</Button>
+      <Stack component="form" gap={4} onSubmit={handleSubmit}>
+        <Typography variant="h3">Register An Acount</Typography>
+      <TextField label="Email"/>
+      <TextField label="Password"/>
+      <TextField label="First name"/>
+      <TextField label="Last name"/>
+     <Button variant="contained" type="submit">Register</Button>
+      </Stack>
     </main>
   );
 }
